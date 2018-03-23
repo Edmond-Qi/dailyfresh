419208bfa28c2002a27bef9863ccf7ec859e2aec
xwp <xwp_fullstack@163.com>
2018-03-23 14:29:34 +0800

    faire une migration pour project de dailyfresh

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'df_user',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,7 +82,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWD': 'mysql',
+        'PASSWORD': 'mysql',
     }
 }
 
